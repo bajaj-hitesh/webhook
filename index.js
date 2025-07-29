5a5a83dfde708da280a87f1c4e8d1499c28a5c47
bajaj-hitesh <hitesh.smit@gmail.com>
2025-07-30 00:23:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-// index.js
-
 /**
- * HTTP Cloud Function to handle webhook events
+ * Your HTTP handling function, invoked with each request. This is an example
+ * function that echoes its input to the caller, and returns an error if
+ * the incoming request is something other than an HTTP POST or GET.
  *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
+ * In can be invoked with 'func invoke'
+ * It can be tested with 'npm test'
+ *
+ * @param {Context} context a context object.
+ * @param {object} context.body the request body if any
+ * @param {object} context.query the query string deserialized as an object, if any
+ * @param {object} context.log logging object with methods for 'info', 'warn', 'error', etc.
+ * @param {object} context.headers the HTTP request headers
+ * @param {string} context.method the HTTP request method
+ * @param {string} context.httpVersion the HTTP protocol version
+ * See: https://github.com/knative/func/blob/main/docs/function-developers/nodejs.md#the-context-object
  */
-exports.webhookListener = (req, res) => {
-  // Only accept POST requests
-  if (req.method !== 'POST') {
-    return res.status(405).send('Method Not Allowed');
+const handle = async (context, body) => {
+  // YOUR CODE HERE
+  context.log.info("query", context.query);
+  context.log.info("body", body);
+
+  // If the request is an HTTP POST, the context will contain the request body
+  if (context.method === 'POST') {
+    return { body };
+  } else if (context.method === 'GET') {
+    // If the request is an HTTP GET, the context will include a query string, if it exists
+    return {
+      query: context.query,
+    }
+  } else {
+    return { statusCode: 405, statusMessage: 'Method not allowed' };
   }
+}
 
-  // Log headers and body for inspection
-  console.log('Headers:', JSON.stringify(req.headers));
-  console.log('Body:', JSON.stringify(req.body));
-
-  // Optional: Validate webhook secret if needed
-  // const receivedSignature = req.headers['x-signature'] || '';
-  // const expectedSignature = createYourHMACFunction(req.body);
-  // if (receivedSignature !== expectedSignature) {
-  //   return res.status(401).send('Invalid signature');
-  // }
-
-  // Process the webhook payload
-  // You can add logic based on event type or content
-
-  res.status(200).send('Webhook received successfully');
-};
+// Export the function
+module.exports = { handle };
